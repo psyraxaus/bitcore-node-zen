bitpay/bitcore-node

Added unit tests for encoding files.

test/services/bitcoind/bitcoind.unit.js
@@ -13,18 +13,18 @@ var proxyquire = require('proxyquire');
 var fs = require('fs');
 var sinon = require('sinon');
 
-var index = require('../../lib');
+var index = require('../../../lib');
 var log = index.log;
 var errors = index.errors;
 
 var Transaction = bitcore.Transaction;
-var readFileSync = sinon.stub().returns(fs.readFileSync(path.resolve(__dirname, '../data/bitcoin.conf')));
-var BitcoinService = proxyquire('../../lib/services/bitcoind', {
+var readFileSync = sinon.stub().returns(fs.readFileSync(path.resolve(__dirname, '../../data/bitcoin.conf')));
+var BitcoinService = proxyquire('../../../lib/services/bitcoind', {
   fs: {
     readFileSync: readFileSync
   }
 });
-var defaultBitcoinConf = fs.readFileSync(path.resolve(__dirname, '../data/default.bitcoin.conf'), 'utf8');
+var defaultBitcoinConf = fs.readFileSync(path.resolve(__dirname, '../../data/default.bitcoin.conf'), 'utf8');
 
 describe('Bitcoin Service', function() {
   var txhex = '01000000010000000000000000000000000000000000000000000000000000000000000000ffffffff0704ffff001d0104ffffffff0100f2052a0100000043410496b538e853519c726a2c91e61ec11600ae1390813a627c66fb8be7947be63c52da7589379515d4e0a604f8141781e62294721166bf621e73a82cbf2342c858eeac00000000';

psyraxaus/bitcore-node-zen

test/services/bitcoind/bitcoind.unit.js
@@ -13,18 +13,18 @@ var proxyquire = require('proxyquire');
 var fs = require('fs');
 var sinon = require('sinon');
 
-var index = require('../../lib');
+var index = require('../../../lib');
 var log = index.log;
 var errors = index.errors;
 
 var Transaction = bitcore.Transaction;
-var readFileSync = sinon.stub().returns(fs.readFileSync(path.resolve(__dirname, '../data/zen.conf')));
-var BitcoinService = proxyquire('../../lib/services/bitcoind', {
+var readFileSync = sinon.stub().returns(fs.readFileSync(path.resolve(__dirname, '../../data/zen.conf')));
+var BitcoinService = proxyquire('../../../lib/services/bitcoind', {
   fs: {
     readFileSync: readFileSync
   }
 });
-var defaultBitcoinConf = fs.readFileSync(path.resolve(__dirname, '../data/default.zen.conf'), 'utf8');
+var defaultBitcoinConf = fs.readFileSync(path.resolve(__dirname, '../../data/default.zen.conf'), 'utf8');
 
 describe('Bitcoin Service', function() {
   var txhex = '01000000010000000000000000000000000000000000000000000000000000000000000000ffffffff0704ffff001d0104ffffffff0100f2052a0100000043410496b538e853519c726a2c91e61ec11600ae1390813a627c66fb8be7947be63c52da7589379515d4e0a604f8141781e62294721166bf621e73a82cbf2342c858eeac00000000';